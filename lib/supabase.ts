479cfbb4025c5d28f26b06846ddfbfaf2bd232c5
larisjayafotocopy-a11y <larisjayafotocopy@gmail.com>
2026-07-09 23:11:11 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.https://xrwhnmljmhbdapzhwvfn.supabase.co;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhyd2hubWxqbWhiZGFwemh3dmZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3OTQ2NjQsImV4cCI6MjA5ODM3MDY2NH0.C4i8Y2yLFP_YuAMOaK2636xfNUVRiqOA2s45HtIJyPU;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
